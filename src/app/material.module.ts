b6c6772615098e8e5a4e2ebb5c575bc25d4f0533
Tomas Vaedelund <tvaedelund@gmail.com>
2018-10-08 10:55:29 +0200

    Added basic app structure with toolbar in footer

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
+// import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule]
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule]
 })
 export class MaterialModule {}
