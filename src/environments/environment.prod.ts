19d39be189d43f0473455130877f60cbd148d606
Tomas Vaedelund <tvaedelund@gmail.com>
2018-10-09 11:33:43 +0200

    Added firebase config to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,15 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyDJzkITNAiY_OIGpxgwNbjOQpYzoGKEBXY',
+    authDomain: 'grocery-7a2cc.firebaseapp.com',
+    databaseURL: 'https://grocery-7a2cc.firebaseio.com',
+    projectId: 'grocery-7a2cc',
+    storageBucket: 'grocery-7a2cc.appspot.com',
+    messagingSenderId: '829896509671'
+  },
   actionCodeSettings: {
-    url: 'http://localhost:4200/login',
+    url: 'https://grocery-7a2cc.firebaseapp.com/login',
     handleCodeInApp: true
   }
 };
