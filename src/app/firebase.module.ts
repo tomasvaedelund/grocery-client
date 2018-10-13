d2975181933d608da447b0ab4a5b3d19bcf212f6
Tomas Vaedelund <tvaedelund@gmail.com>
2018-10-13 10:08:09 +0200

    Cleaning

--- src/app/firebase.module.ts
+++ src/app/firebase.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+
 import { environment } from '../environments/environment';
 
 @NgModule({
