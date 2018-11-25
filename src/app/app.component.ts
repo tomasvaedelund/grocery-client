6a115c2ff76b5220b39d17c721aed3445f40ba6b
Tomas Vaedelund <tvaedelund@gmail.com>
2018-11-25 09:44:13 +0100

    Stuff...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +11,13 @@ export class AppComponent {
   @ViewChild('sidenav')
   sidenav: MatSidenav;
 
-  opened = false;
-  menuIcon = 'menu';
+  opened: boolean;
+  menuIcon: string;
+
+  constructor(public auth: AuthService) {
+    this.opened = false;
+    this.menuIcon = 'menu';
+  }
 
   toggle(): void {
     this.opened = !this.opened;
